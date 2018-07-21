195ee20d8573f1bacabffc7f32fa8eb8c26bce63
Troels lund <trolund@gmail.com>
2018-07-21 20:25:11 +0200

    mail php

---
+++ js/sendmail.js
@@ -0,0 +1,74 @@
+/*
+function send() {
+
+    var name = $("#nameMail").val();
+    var msg = $("#msgMail").val();
+    var email = $("#emailMail").val();
+    var emne = $("#emneMail").val();
+
+    var url = "https://outlook.office.com/api/v2.0/me/sendmail";
+
+    var data = {
+        "Message": {
+            "Subject": emne,
+            "Body": {
+                "ContentType": "Text",
+                "Content": "Navn" + name + "\n" + "email" + email + "\n" + msg
+            },
+            "ToRecipients": [
+                {
+                    "EmailAddress": {
+                        "Address": "trolund@gmail.com"
+                    }
+      }
+    ],
+            "Attachments": [
+                {
+                    "@odata.type": "#Microsoft.OutlookServices.FileAttachment",
+                    "Name": "menu.txt",
+                    "ContentBytes": "bWFjIGFuZCBjaGVlc2UgdG9kYXk="
+      }
+    ]
+        },
+        "SaveToSentItems": "false"
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: function () {
+            console.log("mail sendt!");
+        },
+        dataType: "application/json"
+    });
+}
+*/
+
+$(document).ready(function () {
+
+
+    $('#submit').click(function () {
+
+        console.log('mail send!');
+
+        var name = $('#nameMail').val();
+        var email = $('#emailMail').val();
+
+        var varData = 'name=' + name + '&email=' + email;
+        console.log(varData);
+
+        $.ajax({
+            type: "POST",
+            url: 'php.php',
+            data: varData,
+            success: function () {
+                alert("It was a success");
+            }
+
+        });
+
+    });
+
+
+});
