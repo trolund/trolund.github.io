73f2237b3fd59ae9f714d7aa4927936f31eb0116
Troels lund <trolund@gmail.com>
2018-09-08 14:44:57 +0200

    preloader added

--- js/main.js
+++ js/main.js
@@ -427,3 +427,11 @@ function mobilMenu(x) {
         x.classList.add("change");
     }
 }
+
+$(window).on('load', function () { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
