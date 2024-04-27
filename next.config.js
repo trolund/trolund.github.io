8805321da2a83b3da8aec0b8f14866af0eb3f10d
Troels Lund <trolund@gmail.com>
2024-04-27 11:19:26 +0200

    more

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-const withImages = require("next-images");
-
-module.exports = withImages({
+module.exports = {
+    output: 'export',
     reactStrictMode: true,
     // Add basePath
     // basePath: '/github-pages',
@@ -14,8 +13,8 @@ module.exports = withImages({
     //   disable: process.env.NODE_ENV === "development"
     // },
     images: {
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-      domains: ["trolund.vercel.app", "trolund.github.io"],
+      // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      // domains: ["trolund.vercel.app", "trolund.github.io"],
+      unoptimized: true
     },
   }
-);
