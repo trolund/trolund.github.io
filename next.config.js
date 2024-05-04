8bf66b89d192284daad027659d1f19519d7ea949
Troels Lund <trolund@gmail.com>
2024-05-04 20:37:35 +0200

    more

--- next.config.js
+++ next.config.js
@@ -15,6 +15,5 @@ module.exports = {
     images: {
       // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       // domains: ["trolund.vercel.app", "trolund.github.io"],
-      unoptimized: true
     },
   }
