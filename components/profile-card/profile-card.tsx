75e3332f1f3fb62a90cae79d7356353749ffd61f
Troels Lund <trolund@gmail.com>
2024-06-07 20:06:06 +0200

    more

--- components/profile-card/profile-card.tsx
+++ components/profile-card/profile-card.tsx
@@ -2,17 +2,42 @@ import styles from './profile-card.module.css';
 import Image from 'next/image'
 
 export default function ProfileCard() {
+    function calculateAge(birthdate) {
+        // Parse the birthdate string to create a Date object
+        const birthDate = new Date(birthdate);
+        const today = new Date();
+    
+        // Calculate the age difference in years
+        let age = today.getFullYear() - birthDate.getFullYear();
+    
+        // Adjust the age if the birthdate hasn't occurred yet this year
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+        const dayDifference = today.getDate() - birthDate.getDate();
+    
+        if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+            age--;
+        }
+    
+        return age;
+    }
+
+
     return (
         <figure className={"rounded-xl float-left card p-5 " + styles.container}>
             <div className={styles.img}>
-                <Image src="/profil.webp" alt="me" width="100%" height="100%" layout="responsive" objectFit="contain" />
+                <Image style={{borderTopLeftRadius: "5px", borderTopRightRadius: "5px"}} src="/profil.webp" alt="me" width="100%" height="100%" layout="responsive" objectFit="contain" />
             </div>
             <div className="pt-6 space-y-4">
                 <figcaption>
                     <b style={{ fontSize: "2rem" }}>Troels Lund</b>
-
                     <div>
-                        <i>Software engineer, Denmark</i>
+                    <ol>
+                        
+                        <li><b>Software Engineer</b></li>
+                        <li><b>MSc Eng. Computer Science</b></li>
+                        <li><i>{calculateAge('1994-10-06')} years old</i></li>
+                        <li>Denmark</li>
+                    </ol>
                     </div>
                 </figcaption>
             </div>
