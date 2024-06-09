c6e4ee4aa74530c4774b02d3a50338a6da165c11
Troels Lund <trolund@gmail.com>
2024-06-09 09:29:07 +0200

    moved post badges

--- components/project-item/project-item.tsx
+++ components/project-item/project-item.tsx
@@ -20,8 +20,7 @@ export default function ProjectItem({
             <div className="mb-5">
                 <div className={cn('shadow-small', {
                     'hover:shadow-medium transition-shadow duration-200': slug,
-                }) + " " + "pt-4 pl-2 flex flex-wrap"} style={{ backgroundImage: `url(${coverImage})`, height: "200px", width: "100%", backgroundSize: "cover", backgroundPosition: "center", overflow: "hidden" }}>
-                    {technologies && technologies.map((t, i) => <span key={i} style={{ backgroundColor: "var(--footer)", height: "45px" }} className="px-3 py-2 border-solid border-2 border-gray-800 border-opacity-25 rounded-full mr-2 grow-0">{t}</span>)}
+                })} style={{ backgroundImage: `url(${coverImage})`, height: "200px", width: "100%", backgroundSize: "cover", backgroundPosition: "center", overflow: "hidden" }}>
                 </div>
             </div>
             <h3 className="text-3xl mb-3 leading-snug">
@@ -35,6 +34,9 @@ export default function ProjectItem({
                     <Image src="/assets/flags/da.svg" height={15} width={30}  alt="dansk" /> 
                     : <Image src="/assets/flags/en.svg" height={15} width={30} alt="english"/>}
                 </span>
+            </div>
+            <div className="flex flex-wrap">
+                {technologies && technologies.map((t, i) => <span key={i} style={{ backgroundColor: "var(--footer)", height: "45px" }} className="px-3 py-2 mb-1 border-solid border-2 border-gray-800 border-opacity-25 rounded-full mr-2 grow-0">{t}</span>)}
             </div>
             <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
             {/* <Avatar name={author.name} picture={author.picture} /> */}
