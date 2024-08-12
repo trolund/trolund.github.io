7ab3efd84febeaf1dd159b58d9dfc58253eed42e
Troels Lund <trolund@gmail.com>
2024-08-12 21:21:44 +0200

    bugfix card...

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,17 +5,25 @@ interface CardProp {
     style?: React.CSSProperties;
     className?: String;
     children: React.ReactNode;
-    // href?: URL | string;
+    href?: URL | string;
 }
 
-const Card: React.FC<CardProp> = ({ children, style, className }: CardProp) => {
-    return (
-        // <Link href={href ?? ""} legacyBehavior>
+const Card: React.FC<CardProp> = ({ children, style, className, href }: CardProp) => {
+
+    var container = (
         <div style={style} className={"card-low lg:m-3 md:m-2 sm:m-1 m-1 p-3 " + className ?? ""}>
             {children}
-        </div>
-        // </Link>
-    );
+        </div>)
+
+    if (href) {
+        return (
+            <Link href={href} legacyBehavior>
+                {container}
+            </Link>
+        );
+    }
+
+    return (container)
 }
 
 export default Card;
