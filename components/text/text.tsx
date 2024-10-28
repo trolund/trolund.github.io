27db221420dc41aabbd31e0d8388558d1f742eb2
Troels Lund <trolund@gmail.com>
2024-10-28 19:25:39 +0100

    migrate

--- components/text/text.tsx
+++ components/text/text.tsx
@@ -20,7 +20,6 @@ interface TextProps {
 const Text: FunctionComponent<TextProps> = ({
   input: initInput,
   infinity,
-  onlyWhenVisible,
   containerClassName: textContainerClassName,
   underscoreClassName,
   containerStyles: textContainerStyles,
@@ -37,15 +36,7 @@ const Text: FunctionComponent<TextProps> = ({
   const [showUnderScore, setShowUnderScore] = useState<boolean>(true);
   const [wait, setWait] = useState(initDelay ?? 0);
 
-  let element: HTMLDivElement;
-
   const input: string[] = Array.isArray(initInput) ? initInput : [initInput];
-
-  const isInViewport = (offset = 0) => {
-    if (!element) return false;
-    const top = element.getBoundingClientRect().top;
-    return top + offset >= 0 && top - offset <= window.innerHeight;
-  };
 
   const word = (wordIndex: number, charIndex: number): string => {
     return input[wordIndex].substring(0, charIndex);
@@ -59,49 +50,35 @@ const Text: FunctionComponent<TextProps> = ({
         return;
       }
 
-      if (onlyWhenVisible === undefined || (onlyWhenVisible && isInViewport(0))) {
-        const wordLength = input[wordIndex].length;
+      const wordLength = input[wordIndex].length;
 
-        if (charIndex >= wordLength) {
-          if (wordIndex < input.length - 1) {
-            setWordIndex(wordIndex + 1);
+      if (charIndex >= wordLength) {
+        if (wordIndex < input.length - 1) {
+          setWordIndex(wordIndex + 1);
+          setCharIndex(0);
+        } else {
+          if (infinity) {
+            setWordIndex(0);
             setCharIndex(0);
           } else {
-            if (infinity) {
-              setWordIndex(0);
-              setCharIndex(0);
-            } else {
-              if (!keepUnderscore) setShowUnderScore(false);
-              clearInterval(interval);
-            }
+            if (!keepUnderscore) setShowUnderScore(false);
+            clearInterval(interval);
           }
-        } else {
-          setCharIndex(charIndex + 1);
+        }
+      } else {
+        setCharIndex(charIndex + 1);
 
-          // make a break
-          if (wordBreakTime && charIndex + 1 === input[wordIndex].length) {
-            setWait(wordBreakTime);
-          }
+        // make a break
+        if (wordBreakTime && charIndex + 1 === input[wordIndex].length) {
+          setWait(wordBreakTime);
         }
       }
     }, writeSpeed ?? 1000);
     return () => clearInterval(interval);
-  }, [
-    wordIndex,
-    charIndex,
-    input,
-    onlyWhenVisible,
-    wordBreakTime,
-    writeSpeed,
-    wait,
-    infinity,
-    isInViewport,
-    keepUnderscore,
-  ]);
+  }, [wordIndex, charIndex, input, wordBreakTime, writeSpeed, wait, infinity, keepUnderscore]);
 
   return (
     <div
-      ref={(el) => (element = el)}
       style={{ ...textContainerStyles, color: color }}
       className={
         styles.textContainer + (textContainerClassName ? ' ' + textContainerClassName : '')
