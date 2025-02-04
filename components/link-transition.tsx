c2fde22cc6f851a9735fcbbab0749e1c5052cf6b
Troels <trolund@gmail.com>
2025-02-04 10:29:48 +0100

    more

--- components/link-transition.tsx
+++ components/link-transition.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 function LinkTransition(props: any) {
   const router = useRouter();
   const handleClick = (e: Event) => {
@@ -11,7 +15,15 @@ function LinkTransition(props: any) {
       // browser supports view transition. Animate the transtion.
       e.preventDefault();
       document.startViewTransition(async () => {
+        const body = document.querySelector('body');
+
+        body?.classList.add('page-transition');
+
+        await sleep(500);
         router.push(props.href as any);
+        await sleep(500);
+
+        body?.classList.remove('page-transition');
       });
     }
   };
