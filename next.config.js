f1b0df4d9f3ae91c778cc9ea0bef2eaabea0fd13
Troels <trolund@gmail.com>
2025-04-19 08:55:05 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    NEXT_PUBLIC_CRONITORIO_CLIENT_KEY: process.env.NEXT_PUBLIC_CRONITORIO_CLIENT_KEY,
+  },
   output: 'export',
   reactStrictMode: true,
   images: {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ type AppProps = {
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useCronitor(process.env.NEXT_PUBLIC_CRONITORIO_CLIENT_KEY ?? '');
+  console.log('client key:', process.env.NEXT_PUBLIC_CRONITORIO_CLIENT_KEY);
   return (
     <ThemeProvider>
       <Head>
