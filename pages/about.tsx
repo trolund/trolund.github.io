ac5f267d961457a0989710f6bd2bf74e118f55cf
Troels <trolund@gmail.com>
2025-04-21 23:13:51 +0200

    more

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,8 +11,6 @@ import styles from '../pages/css/about.module.css';
 import SubPostTitle from '../components/sub-post-title';
 import CertificationItem from '../components/certification-item';
 import { TITLE } from '../lib/constants';
-import Rocket from '../components/rocket/rocket';
-import RocketScene from '../components/rocket/RocketScene';
 
 type AboutProps = {
   page: {
