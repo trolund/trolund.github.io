2fca09fc2c0de03524d044e446a2087b40a1a764
Troels Lund <trolund@gmail.com>
2025-05-20 15:34:49 +0200

    installed react-scan to use on dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import { ThemeProvider } from '../hooks/ThemeContext';
 import '../styles/index.css';
 import Toast from '../components/toast';
@@ -13,6 +14,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   useCronitor(process.env.NEXT_PUBLIC_CRONITORIO_CLIENT_KEY ?? '');
   return (
     <ThemeProvider>
+      {process.env.NODE_ENV === 'development' && (
+        <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />
+      )}
       <Meta />
       <Component {...pageProps} />
       <Toast />
