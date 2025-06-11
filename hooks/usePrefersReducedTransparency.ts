a40b0d6d8a2985a29720ba3a357aecc36cb4c75d
Troels <trolund@gmail.com>
2025-06-11 14:08:02 +0200

    added prefers-reduced-transparency options

---
+++ hooks/usePrefersReducedTransparency.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export const usePrefersReducedTransparency = () => {
+  const [prefersReducedTransparency, setPrefersReducedTransparency] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-reduced-transparency: reduce)');
+
+    // Set the initial value
+    setPrefersReducedTransparency(mediaQuery.matches);
+
+    // Define a handler to update the value when it changes
+    const handleChange = () => {
+      setPrefersReducedTransparency(mediaQuery.matches);
+    };
+
+    // Add listener
+    mediaQuery.addEventListener('change', handleChange);
+
+    // Cleanup
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange);
+    };
+  }, []);
+
+  return prefersReducedTransparency;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,16 @@ import NightParticleCanvas from '../components/canvas/NightParticleCanvas';
 import { useTheme } from '../hooks/ThemeContext';
 import NoiseParticleCanvas from '../components/canvas/NoiseParticleCanvas';
 import { usePrefersReducedMotion } from '../hooks/usePrefersReducedMotion';
+import { usePrefersReducedTransparency } from '../hooks/usePrefersReducedTransparency';
 
 export default function Index() {
   const { isDark } = useTheme();
   const prefersReducedMotion = usePrefersReducedMotion();
+  const reduceTransparency = usePrefersReducedTransparency();
 
   return (
     <>
-      <NavBar items={menu} noBackground />
+      <NavBar items={menu} noBackground={!reduceTransparency} />
       <Head>
         <title>{TITLE}</title>
       </Head>
