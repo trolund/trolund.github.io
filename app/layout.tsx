0e16a54583ed6e80274d06c62503a18d02a30fdd
Troels <trolund@gmail.com>
2025-06-15 17:01:14 +0200

    Implement code changes to enhance functionality and improve performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,60 @@
 import '@/styles/index.css';
 import Toast from '@/components/toast';
-import { ReactNode, StrictMode } from 'react';
+import { ReactNode, StrictMode, Suspense } from 'react';
 import ClientInit from './client-init';
 import { ThemeProvider } from 'next-themes';
-import Head from '@/components/head';
+import { Metadata } from 'next';
+import { TITLE } from '@/lib/constants';
+
+const description = 'A personal portfolio site featuring projects and a blog focused on web development, programming, and technology.';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://troelslund.dk'),
+  title: TITLE,
+  applicationName: TITLE,
+  description: description,
+  icons: {
+    icon: [
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: '/icons/apple-touch-icon.png',
+  },
+  manifest: '/manifest.json',
+    openGraph: {
+    title: TITLE,
+    description: description,
+    images: [
+      {
+        url: '/home.png', 
+        width: 1200,
+        height: 630,
+        alt: "The homepage of Troels Lund's portfolio site",
+      },
+    ],
+  },
+  twitter: {
+    creator: '@trolund',
+    card: 'summary_large_image',
+    title: TITLE,
+    description: 'Your site description',
+    images: ['/home.png'],
+  },
+  other: {
+    'google-site-verification': 'fK40wZmELLwBjymUVMTij0LHyohVxm-xrNj4T7mPzaU',
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <Head />
       <body>
         <StrictMode>
           <ThemeProvider enableSystem={true}>
             {children}
-            <ClientInit />
+            <Suspense fallback={null}>
+              <ClientInit />
+            </Suspense>
             <Toast />
           </ThemeProvider>
         </StrictMode>
