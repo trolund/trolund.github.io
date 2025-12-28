b90f3b3295651d72ebc19bb4e69b9e0ab087cfde
Troels <trolund@gmail.com>
2025-12-28 17:22:56 +0100

    Format SubPostTitle component for improved readability

--- components/sub-post-title.tsx
+++ components/sub-post-title.tsx
@@ -5,7 +5,10 @@ type SubPostTitleProps = {
 
 export default function SubPostTitle({ children, id }: SubPostTitleProps) {
   return (
-    <h2 id={id} className="mb-12 text-center text-4xl font-bold leading-tight tracking-tighter md:text-left md:text-5xl md:leading-none lg:text-6xl">
+    <h2
+      id={id}
+      className="mb-12 text-center text-4xl font-bold leading-tight tracking-tighter md:text-left md:text-5xl md:leading-none lg:text-6xl"
+    >
       {children}
     </h2>
   );
